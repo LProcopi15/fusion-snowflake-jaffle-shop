3f5f0619e70d130fb12518643d3a3b30a8db3638
Leah Procopi <leah.procopi@fishtownanalytics.com>
2024-10-23 01:25:45 +0000

    create first iceberg table

---
+++ models/marts/dim_locations_iceberg.sql
@@ -0,0 +1,17 @@
+{{
+  config(
+    materialized = "table",
+    table_format="iceberg",
+    external_volume="s3_lprocopi_iceberg_encrypted",
+  )
+}}
+
+with
+
+locations as (
+
+    select * from {{ ref('stg_jaffle_shop__locations') }}
+
+)
+
+select * from locations
